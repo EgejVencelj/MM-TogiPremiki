60e3de2318b902b55d8916d070c1ecb3bffda010
Egej Vencelj <egej.vencelj@gmail.com>
2015-04-15 22:27:17 +0200

    NEW: Fist .m file added

---
+++ src/premikiTogihTeles.m
@@ -0,0 +1,17 @@
+function [A b] = premikiTogihTeles(X, Y)
+  vsotaTockX = sum(X')/size(X,2);
+  vsotaTockY = sum(Y')/size(Y,2);
+  X=[X(1,:).-vsotaTockX(1);X(2,:).-vsotaTockX(2);X(3,:).-vsotaTockX(3)];
+  Y=[Y(1,:).-vsotaTockY(1);Y(2,:).-vsotaTockY(2);Y(3,:).-vsotaTockY(3)];
+  C = Y*X';
+  [U S V] = svd(C);
+  determinanta = det(C);
+  D = eye(3);
+  b = vsotaTockY-vsotaTockX;
+  b = sqrt(b*b');
+  if(determinanta < 0)
+     D(3,3) = -1;
+  endif
+  A = U*D*V';
+  %plot3(Y(1,:),Y(2,:),Y(3,:),'bo',X(1,:), X(2,:),X(3,:),'*')
+end
