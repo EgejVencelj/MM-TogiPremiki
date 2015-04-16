df4e434746cb463370dc6d3c4e2ba0b850ec52f8
Egej <egej.vencelj@ibzoot.com>
2015-04-16 11:25:11 +0200

    NEW: Reset button

---
+++ src/Togi Premik/Assets/Manipulation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Manipulation : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public void Reset()
+	{
+		transform.rotation = Quaternion.identity;
+		transform.position = Vector3.zero;
+		transform.localScale = Vector3.one;
+	}
+}
