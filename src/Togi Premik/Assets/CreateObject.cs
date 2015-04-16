5d678a48c7f7ffcb93bff0778dd227c015bf6c9e
Egej <egej.vencelj@ibzoot.com>
2015-04-16 20:21:40 +0200

    NEW: Things

---
+++ src/Togi Premik/Assets/CreateObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateObject : MonoBehaviour
+{
+	GameObject go;
+	public InputField[] fields;
+
+	// Use this for initialization
+	void Start()
+	{
+		go = new GameObject();
+		go.AddComponent<LineRenderer>();
+		go.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public void Create()
+	{
+		go.SetActive(true);
+		LineRenderer lr = go.GetComponent<LineRenderer>();
+		lr.SetWidth(0.1f, 0.1f);
+		lr.SetVertexCount(fields.Length / 3);
+		for (int i = 0; i < fields.Length; i += 3)
+		{
+			lr.SetPosition(i / 3, new Vector3(float.Parse(fields[i].text), float.Parse(fields[i + 1].text), float.Parse(fields[i + 2].text)));
+			//lr.SetPosition(i / 3, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)));
+		}
+	}
+}
--- src/Togi Premik/Assets/Manipulation.cs
+++ src/Togi Premik/Assets/Manipulation.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manipulation : MonoBehaviour
 {
+	public Transform[] points;
+	public Text Display1;
+	public Text Display2;
 	// Use this for initialization
 	void Start()
 	{
+		string txt = "";
+		foreach (Transform t in points)
+		{
+			txt = string.Format("{0}{1}\n", txt, t.position.ToString());
+		}
+		Display1.text = txt;
 	}
 
 	// Update is called once per frame
@@ -13,7 +23,17 @@ public class Manipulation : MonoBehaviour
 	{
 	}
 
-	public void Reset()
+	public void UpdatePosition()
+	{
+		string txt = "";
+		foreach (Transform t in points)
+		{
+			txt = string.Format("{0}{1}\n", txt, t.position.ToString());
+		}
+		Display2.text = txt;
+	}
+
+	public void ResetPosition()
 	{
 		transform.rotation = Quaternion.identity;
 		transform.position = Vector3.zero;
